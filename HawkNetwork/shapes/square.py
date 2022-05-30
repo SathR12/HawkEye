01f6a2a43f8f8b7c073b648990d16b53e329bbc4
Sathya <74515743+SathR12@users.noreply.github.com>
2022-05-29 20:12:49 -0400

    Delete HawkNetwork directory

--- HawkNetwork/shapes/square.py
+++
@@ -1,8 +0,0 @@
-import cv2 as cv
-
-def isSquare(contour):
-    approx = cv.approxPolyDP(contour, 0.01 * cv.arcLength(contour, True), True)
-    if len(approx) == 4 or len(approx) == 5:
-        return True
-    
-    return False 
